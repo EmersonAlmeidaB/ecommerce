04ea7237db093a104460c7346b9575ff892c584a
EmersonAlmeidaB <emerson8204@hotmail.com>
2023-05-11 23:00:20 -0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+async function getproducts() {
+    try {
+        const data = await fetch(
+            "https://ecommercebackend.fundamentos-29.repl.co/"
+        );
+
+        const res = await data.json();
+        window.localStorage.setItem("products",JSON.stringify(res));
+    
+        return res;
+    
+    } catch (error) {
+        console.log(error);
+    }     
+}
+
+function printProducts(db) {
+    const productsHTML = document.querySelector(".products");
+
+    let html = "";
+
+    for (const product of db.products) {
+        html += `
+        <div class="product">
+            <div class="product__img">
+                <img src="${product.image}" alt="image"/>
+            </div>
+            <div class="product__info">
+                <i class='bx bx-plus' id='${product.id}'></i>
+                <h5>$${product.price}US  | <span><Stock</b>Stock ${product.quantity}</span>
+                </h5>
+                <h4>${product.name}
+                
+                </h4>
+            </div>
+        
+        </div>
+        
+        `;
+    }
+
+    productsHTML.innerHTML = html;
+
+    
+}
+
+async function main() {
+    const db = {
+        products: JSON.parse(window.localStorage.getItem("products")) || (await getproducts()),
+        cart:{},
+    } 
+    printProducts(db)
+    
+    
+};
+main();
